cc79f3e24aa8ffdae85cafe92215bf2c135cb470
FelipeMacedo315 <felipemacedo315@gmail.com>
2023-10-11 17:25:39 -0300

    route put client finally

--- src/main/java/com/example/FelipeAutoCenter/Controllers/ClientsController.java
+++ src/main/java/com/example/FelipeAutoCenter/Controllers/ClientsController.java
@@ -5,27 +5,18 @@ import com.example.FelipeAutoCenter.Services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriBuilder;
+
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 
-import static org.springframework.web.util.UriComponentsBuilder.fromUri;
+
 
 @RestController
 public class ClientsController {
     @Autowired
     private ClientService clientService;
-
-    @PostMapping(path = "/clients/create-client")
-    public ResponseEntity receiverInfoClient(@RequestBody ClientsEntities client) throws URISyntaxException {
-        clientService.insertClient(client);
-        URI baseURL = new URI("http://localhost:8080/client/");
-        URI pathExactUser = URI.create(baseURL + client.getId().toString());
-        return ResponseEntity.created(pathExactUser).build();
-
-    }
 
     @GetMapping(path = "/clients/all-clients")
     public ResponseEntity showAllClients() {
@@ -33,5 +24,22 @@ public class ClientsController {
         return ResponseEntity.ok(result);
     }
 
+    @PostMapping(path = "/clients/create-client")
+    public ResponseEntity receiverInfoClient(@RequestBody ClientsEntities client) throws URISyntaxException {
+        clientService.insertClient(client);
+        URI baseURL = new URI("http://localhost:8080/clients/exact-client/");
+        URI pathExactUser = URI.create(baseURL + client.getId().toString());
+        return ResponseEntity.created(pathExactUser).build();
+    }
+
+    @PutMapping(path = "/clients/update-client/{id}")
+    public ResponseEntity UpdateClient(@PathVariable Long id, @RequestBody ClientsEntities client) {
+        Boolean response = clientService.update(id, client);
+        if (response) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(404).body("Client not find");
+        }
+    }
 
 }
--- src/main/java/com/example/FelipeAutoCenter/Services/ClientService.java
+++ src/main/java/com/example/FelipeAutoCenter/Services/ClientService.java
@@ -2,15 +2,19 @@ package com.example.FelipeAutoCenter.Services;
 
 import com.example.FelipeAutoCenter.Entities.ClientsEntities;
 import com.example.FelipeAutoCenter.repository.ClientsRepository;
+import org.aspectj.util.LangUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.integration.IntegrationProperties;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
 public class ClientService {
-
+    ClientsEntities clientsEntities = new ClientsEntities();
     @Autowired
     private ClientsRepository clientsRepository;
 
@@ -18,7 +22,19 @@ public class ClientService {
         clientsRepository.save(client);
 
     }
-    public List showAllClients(){
-       return  clientsRepository.findAll();
+
+    public List showAllClients() {
+        return clientsRepository.findAll();
+    }
+
+    public Boolean update(Long id, ClientsEntities client) {
+        Optional checkExist = clientsRepository.findById(id);
+        if (checkExist.isPresent()) {
+            client.setId(id);
+            clientsRepository.save(client);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
